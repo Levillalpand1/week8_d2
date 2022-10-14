3a7330cb273558d6be204a3ad02cdae34ddc5d29
Luis Villalpando <113930826+Levillalpand1@users.noreply.github.com>
2022-10-14 17:20:34 +0000

    I added colors

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from random import randint
 from random import shuffle
+from random import choice
 # random
 # Python comes with a built in random library. There are a lot of functions included in this random library, so we will only  #show you two useful functions for now.
 # print("random")
@@ -36,3 +37,6 @@ else:
 for num in range(2,201):
   if num % 2==0:
     print(num)
+color=["red","blue","pink","purple"]
+random_color= choice(color)
+print(f"random color is {random_color}")
