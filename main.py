2a87083fb1cace532bd117931de58009c1eebe99
Luis Villalpando <113930826+Levillalpand1@users.noreply.github.com>
2022-10-14 17:14:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+
+from random import randint
+from random import shuffle
+# random
+# Python comes with a built in random library. There are a lot of functions included in this random library, so we will only  #show you two useful functions for now.
+# print("random")
+# dice1=randint(1,7)
+# print(f"dice1:{dice1}")
+# dice2=randint(1,7)
+# print(f"dice2:{dice2}")
+# rolledDoubles = dice1 == dice2
+# if rolledDoubles:
+#    print("you rolled doubles!")
+# else: print("not doubles")
+
+# if dice1 and dice2==1:
+#   print("you rolled snake eyes")
+# else: print("you didn't roll snake eyes!")
+
+# my_list=range(1,51)
+# print ("my new list")
+# print(list(my_list))
+# my_list= list(my_list)
+# (shuffle(my_list))
+# print(my_list)
+
+random=randint(1,201)
+print("random")
+print(random)
+
+if random % 2== 0:
+  print("even numeber!")
+else:
+  print("odd number")
+
+for num in range(2,201):
+  if num % 2==0:
+    print(num)
